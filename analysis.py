0acda305078a39ba4227177f957342c50880ac49
HARSHA789SIDDAM <72926700+HARSHA789SIDDAM@users.noreply.github.com>
2021-07-06 18:00:51 +0530

    Add files via upload

---
+++ analysis.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import csv
+import plotly_express as px
+
+df = pd.read_csv(
+    "E:\python classes\pro c-107\data.csv")
+
+data = df.groupby(['level', 'student_id'], as_index=False).mean()
+
+fig = px.scatter(data, x="student_id", y="level", color="attempt", size="attempt",
+                 title="The Average Attempts of Each Student In Each Level")
+
+fig.show()
